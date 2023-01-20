51b794022e9605e3758b00a8e9e42ecfce0181eb
jsparks404 <sparksj404@gmail.com>
2023-01-20 13:22:43 -0500

    delivery model

---
+++ models/delivery.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const DeliverySchema = new mongoose.Schema({
+    idNumber: Number,
+    idNumber2: Number,
+    address: String,
+    city: String,
+    state: String,
+    zip: Number,
+    notes: String
+}, {timestamps: true})
+
+
+const Deliveries = mongoose.model('Deliveries', DeliverySchema)
+
+module.exports = Deliveries
