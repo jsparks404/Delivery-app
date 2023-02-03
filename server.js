bbc0551f665e22e095fbec48f9c2dd37132919ab
jsparks404 <sparksj404@gmail.com>
2023-02-03 14:52:09 -0500

    app on fly.io but cannot access. my be port problem

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 const methodOverride = require('method-override')
 const express = require('express')
 const app = express()
-const PORT = 4000
-
+require('dotenv').config()
+const PORT = process.env.PORT || 8080
 
 
 const deliveriesController = require('./controllers/deliveryController')
